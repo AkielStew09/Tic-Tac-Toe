c759eff231dee11293fb191d2098f7fdcb2bdb89
Akiel Stewart <140494913+AkielStew09@users.noreply.github.com>
2025-08-21 23:35:41 -0500

    Made the Square factory and finished writing up the Gameboard. I changed the function for printing the board to the console as well. Next will be to write the game and round logic.

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
-const Gameboard = () => {
+const Gameboard = (() => {
     //we'll have the array be nine elements long with values of
     //either 'x', 'o', or '-' for empty.
     let boardArr = [];
     const setUpBoard = () => {
         for (let i = 0; i < 9; ++i) {
-            boardArr[i] = '-';
+            boardArr[i] = Square();
         }
     };
-
-    const printBoard = ()=>{
-        let printArr = [];
-        //I'm trying to split printArr into three separate arrays of three
-        for(let i = 0; i < 3; ++i){
-            printArr[i] = [];
-
-            for(let j = 0; j < 3; ++j){
-                printArr[i].push(boardArr[i]);     
-            }
-
-            console.log(printArr[i]);
-        }
-    }
+    
     const getBoard = ()=> boardArr;
 
+    const printBoard = ()=>{
+        
+        for(let i = 0; i < 3; ++i){
+           
+            //slice(start, end - not including end)
+            //first example: slice(0, 3)
+            console.log(boardArr.slice(i*3, i*3 +3));
+        }
+    }
+    
     const acceptMove = (player, indexOfMove)=>{
-        let cell = boardArr[indexOfMove];
+        //retrieve the Square object at that index
+        let square = boardArr[indexOfMove];
 
-        if(cell )
-            cell = player.symbol;
+        if(square.getSymbol() === '-' ){
+            square.acceptSymbol(player);
+        } 
     }
 
-    return { getBoard, printBoard };
-};
+    return { setUpBoard, getBoard, printBoard, acceptMove };
+})();//turned this to an IIFE, my first ever one in fact.
+
+const Square = ()=>{
+    let symbol = '-';
+
+    //function to be drawn in by a player. A setter, essentially.
+    const acceptSymbol =(player)=>{
+        if (player.symbol === 'x' || player.symbol === 'o' ){
+            symbol = player.symbol;
+        }else{
+            console.error("Invalid symbol for Tic-Tac-Toe. Please use an 'x' or 'o'.");
+        }
+    }
+
+    //getter
+    const getSymbol = ()=>symbol;
+
+    return{acceptSymbol, getSymbol};
+}
 
 function Player(name, symbol) {
     let score = 0;
