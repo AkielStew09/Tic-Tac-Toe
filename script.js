143b3cae5fad932f4815a8f477c9e0c5b2c2d219
Akiel Stewart <140494913+AkielStew09@users.noreply.github.com>
2025-08-21 22:26:25 -0500

    Changed the empty character for the board from e(for empty) to a simple hyphen. We'll see how that pans out.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const Gameboard = () => {
     //we'll have the array be nine elements long with values of
-    //either 'x', 'o', or 'e' for empty.
+    //either 'x', 'o', or '-' for empty.
     let boardArr = [];
     const setUpBoard = () => {
         for (let i = 0; i < 9; ++i) {
-            boardArr[i] = 'e';
+            boardArr[i] = '-';
         }
     };
 
     const printBoard = ()=>{
         let printArr = [];
+        //I'm trying to split printArr into three separate arrays of three
         for(let i = 0; i < 3; ++i){
             printArr[i] = [];
-            
+
             for(let j = 0; j < 3; ++j){
                 printArr[i].push(boardArr[i]);     
             }
